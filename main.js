d12b8aab045e4064b3c85c9c813b3c12e3a41f78
Nikita <nikitann2501@gmail.com>
2022-06-29 22:55:20 +0530

    conditional rendering in vue

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function () {
+        return {
+            product: 'Socks',
+            productDescription: 'Summer half socks',
+            image: './assets/images/white.jpg',
+            inventory: 100,
+            details:['50% cotton', '30% wool', '20% polyster'],
+            varients:[
+                {
+                    id:2234, color:'white'
+                },
+                {
+                    id:2235, color:'gray'
+                },
+                {
+                    id:2236, color:'multi color'
+                }
+            ]
+            // url:'https://v2.vuejs.org/v2/guide/'
+        }
+    }
+})
