2804c7a9ca9c8394d4a80707628b567db30b23cd
Nikita <nikitann2501@gmail.com>
2022-06-30 21:35:59 +0530

    classes and style binding

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 const app = Vue.createApp({
     data: function () {
         return {
+            cart: 0,
             product: 'Socks',
-            productDescription: 'Summer half socks',
+            // productDescription: 'Summer half socks',
             image: './assets/images/white.jpg',
-            inventory: 100,
-            details:['50% cotton', '30% wool', '20% polyster'],
-            varients:[
-                {
-                    id:2234, color:'white'
-                },
-                {
-                    id:2235, color:'gray'
-                },
-                {
-                    id:2236, color:'multi color'
-                }
+            inStock: true,
+            // inventory: 100,
+            details: ['50% cotton', '30% wool', '20% polyster'],
+            variants: [
+                { id: 2234, color: 'white', image: './assets/images/white.jpg' },
+                { id: 2235, color: 'gray', image: './assets/images/gray.jpg' },
+                { id: 2236, color: 'blue', image: './assets/images/blue.jpg' }
             ]
             // url:'https://v2.vuejs.org/v2/guide/'
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(varientImage) {
+            this.image = varientImage
         }
     }
 })
